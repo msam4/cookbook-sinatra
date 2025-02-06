2858b11e5e568fbe3d6696784fd70bea8c0e5764
michaelsam <137851066+msam4@users.noreply.github.com>
2025-02-06 20:16:09 +0900

    Add inital pseudocode

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 
-
+# Index goes in here
 get "/" do
   @usernames = ["ssaunier", "Papillard"]
   erb :index
@@ -16,3 +16,13 @@ get "/team/:username" do
   puts params[:username]
   "The username is #{params[:username]}"
 end
+
+# Add a create
+# get "/create" do
+#   # Create goes here
+# end
+
+# Add a destroy
+# get "/destroy" do
+#   # Destroy goes here
+# end
